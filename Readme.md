892d1905407eb360995dd9fb70849eabca6a87c9
Purtv <Alexpurtv@gmail.com>
2021-06-04 04:08:38 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# simple enot io sdk
+
+## Install
+    composer require purt09/enot
+## Unit testing
+### Install in your local
+    $ composer install
---
+++ src/Enot.php
@@ -0,0 +1,51 @@
+<?php
+
+
+class Enot
+{
+    const LINK = "https://enot.io/pay?";
+
+    private $id;
+
+    private $secret_key;
+
+    private $secret_key_2;
+
+    public function __construct(int $id, string $secret_key, string $secret_key_2)
+    {
+        $this->id = $id;
+        $this->secret_key = $secret_key;
+        $this->secret_key_2 = $secret_key_2;
+    }
+
+    public function generateSign(float $order_amount, string $payment_id): string
+    {
+        return md5($this->id.':'.$order_amount.':'.$this->secret_key.':'.$payment_id);
+    }
+
+    public function generateLink(float $sum, string $payment_id, $spec_value = "spec", string $currency = "RUB", string $comment = "comment")
+    {
+        $params = [
+            'm' => $this->id,
+            'oa' => $sum,
+            'o' => $payment_id,
+            's' => $this->generateSign($sum, $payment_id),
+            'cf' => $spec_value,
+            'cr' => $currency,
+            'c' => $comment
+        ];
+        return self::LINK . http_build_query($params);
+    }
+
+    /**
+     * @param array $params
+     */
+    public function checkSign(array $params)
+    {
+        $sign = md5($this->id.':'.$params['amount'].':'.$this->secret_key_2.':'.$params['merchant_id']);
+
+        if ($sign != $params['sign_2']) {
+            throw new DomainException('sign_2 not valid');
+        }
+    }
+}
